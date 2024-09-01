4080d6a070265961e3bbc365c95ba656345bbeb8
SultanTipa1 <josephngumbaugush@gmail.com>
2024-09-01 12:30:58 +0300

    Adds all files

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+def countdown(seconds):
+    while seconds > 0:
+        print(f"{seconds} SECOND(S)!")
+        seconds -= 1
+    print("HAPPY NEW YEAR!")
+
+
+def countdown_with_sleep(seconds):
+    import time
+    while seconds > 0:
+        print(f"{seconds} SECOND(S)!")
+        time.sleep(1)
+        seconds -= 1
+    print("HAPPY NEW YEAR!")
--- lib/debug.py
+++ lib/debug.py
@@ -4,3 +4,7 @@ from countdown import countdown, countdown_with_sleep
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+    pass
+
+
+
